1f13728998ef4f1b53fcb6e1ceb3dd16be79a3cb
jacobrees <jacob.rees97@yahoo.com>
2021-03-12 10:21:06 +0000

    Fix all linting errors within app.js file

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const etchBoard = document.querySelector(".etch-board")
-let boardSize = 16*16
-let etchDiv = ``
+const etchBoard = document.querySelector('.etch-board');
+const boardSize = 16 * 16;
+let etchDiv = '';
 
-for(let i = 0; i < boardSize; i++){
-  etchDiv += `<div class="etch-square"></div>`
+for (let i = 0; i < boardSize; i += 1) {
+  etchDiv += '<div class="etch-square"></div>';
 }
-etchBoard.innerHTML = etchDiv
+etchBoard.innerHTML = etchDiv;
 
-const etchSquare = document.querySelectorAll('.etch-square')
-etchSquare.forEach(function(square){
-  square.style.width = `${550/16 - 2}px`
-  square.style.height = `${550/16 - 2}px`
+const etchSquare = document.querySelectorAll('.etch-square');
+etchSquare.forEach((square) => {
+  square.style.width = `${550 / 16 - 2}px`;
+  square.style.height = `${550 / 16 - 2}px`;
 })
 
-.style.width = `${550/16}px`
-etchSquare.style.height = `${550/16}px`
-
+  .style.width = `${550 / 16}px`;
+etchSquare.style.height = `${550 / 16}px`;
